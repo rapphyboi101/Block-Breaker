b53022d71b4a6cb58ab73c0c33ecb551fdab05c0
Raphael Gonzales <rapramgon@gmail.com>
2019-12-07 13:00:23 +0800

    Block Breaker w/ block breaking sounds

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    //config parameters
+    [SerializeField] AudioClip breakSound;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMin = 1f;
+    [SerializeField] float xMax = 15f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseePosInUnits = (Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x =  Mathf.Clamp(mouseePosInUnits, xMin, xMax);
+        transform.position = paddlePos;
+	}
+}
